9b764d485c96631ce0c34d3fc4b69201000800f7
samfefeomoyeni-lgtm <samfefe.omoyeni@gmail.com>
2026-06-02 10:03:25 +0100

    url fix

--- frontend/my-app/src/api/authApi.js
+++ frontend/my-app/src/api/authApi.js
@@ -1,6 +1,7 @@
 import axiosInstance from './axiosInstance'
 
-export const register = (data) => axiosInstance.post('/api/auth/register/', data)
-export const login = (data) => axiosInstance.post('/api/auth/login/', data)
-export const logout = () => axiosInstance.post('/api/auth/logout/')
-export const getMe = () => axiosInstance.get('/api/auth/me/')
+export const register = (data) => axiosInstance.post('/api/auth/register_user/', data)
+export const login = (data) => axiosInstance.post('/api/auth/login_user/', data)
+export const logout = () => axiosInstance.post('/api/auth/logout_user/')
+export const getStudent = () => axiosInstance.get('/api/student/get_student_data/')
+export const getCafeteria = () => axiosInstance.get('/api/cafeteria/get_cafeteria_data/')
